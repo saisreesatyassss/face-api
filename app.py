7e54a1be8251c5dc919a2be64eb584d765034c46
saisreesatyassss <saisreesatyassss@gmail.com>
2024-11-21 16:10:38 +0530

    crs

--- app.py
+++ app.py
@@ -260,8 +260,10 @@ from flask import Flask, request, jsonify
 import requests
 import PyPDF2
 import io
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def extract_text_from_pdf_bytes(pdf_bytes):
     """
